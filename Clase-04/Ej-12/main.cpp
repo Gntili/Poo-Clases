59e35b92eba9e54df93bf3b56c69827146204d7e
Gntili <santi7gentili@gmail.com>
2024-06-19 03:32:00 -0300

    Add files via upload

---
+++ Clase-04/Ej-12/main.cpp
@@ -0,0 +1,27 @@
+#include <QApplication>
+#include <QLabel>
+#include <QPixmap>
+#include <QTimer>
+
+int main(int argc, char *argv[]) {
+    QApplication a(argc, argv);
+
+    // Crear un objeto QLabel
+    QLabel label;
+
+    // Cargar la imagen desde un archivo JPG en el disco duro
+    QPixmap imagen("/Users/Santiago/Desktop/cielo.jpg");
+    // Escalar la imagen para que se ajuste al tamaño del QLabel sin deformarse
+    label.setPixmap(imagen.scaled(label.size(), Qt::KeepAspectRatio));
+
+    // Mostrar el QLabel maximizado
+    label.showMaximized();
+
+    // Configurar un temporizador para cerrar la aplicación después de 3 segundos
+    QTimer::singleShot(3000, [&](){
+        label.close(); // Cerrar el QLabel
+        a.quit();      // Cerrar la aplicación
+    });
+
+    return a.exec();
+}
